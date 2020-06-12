16c5340b2f70f7b2276b588aec7908762fb81a48
Kamila Ribeiro Melo <kamilamelo5@hotmail.com>
2020-06-12 13:13:41 -0300

    Primeiro exercício de exercício de interpretação de código concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,1 +1,9 @@
 /*Exercícios de interpretação de código*/
+/*NÚMERO 1*/
+//Ele converte o valor de dólar para real. É passado uma função chamada conversorDeMoeda com parâmetro valorEmDolar,
+//dentro dela é solicitada ao usuário a cotação do dólar, e é armazenada em uma variável constate chamada cotacao.
+//E o retorno dessa funçao é o valor em reais. Do lado de fora da função, ela é chamada indicando valor de 100 dólares.
+//O resultado é armazenado dentro de uma variável constante chamada meuDinheiro. Na próxima linha é dado um
+//console.log para imprimir o resultado. O valor impresso no console, caso o usuário de o usuario digitar 5 na cotacao, é
+//R$ 500
+
