e925604e22d2e557cdb8c3e085dfe03c451c2d1b
Kamila Ribeiro Melo <kamilamelo5@hotmail.com>
2020-06-12 13:15:54 -0300

    Terceiro exercício de interpretação de código concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -15,3 +15,12 @@
 //é dado dois console.log para imprimir o resultado das variáveis. O valor impresso no console é 165 e TIPO DE INVESTIMENTO INFORMADO
 //INCORRETO!
 
+/*NÚMERO 3*/
+//Separa em arrays diferentes os números pares e números ímpares. É dado um array com alguns números e outros dois arrays vazios.
+//É chamado um for para verificar todos os elementos do array são pares, se for é jogado para o array1, se eles forem ímpar são jogados no
+//array2. Nas próximas linhas tem 3 console.log, o primeiro para imprimir a quantidade total de numeros dentro do array principal, o segundo
+//para imprimir a quantidade total de numeros pares e o terceiro para imprimir a quantidade total de numeros ímpares. O valor impresso é
+//Quantidade total de números 14
+//6
+//8
+
