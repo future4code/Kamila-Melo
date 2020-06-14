88fa6ae309d4ef391e1799d6563541a0b2f7295f
Kamila Ribeiro Melo <kamilamelo5@hotmail.com>
2020-06-14 14:25:52 -0300

    Terceiro exercício de Funções de array concluído

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -260,13 +260,52 @@
 // console.log(numerosMulti3)
 
 /*LETRA C*/
-const array = [1, 2, 3, 4, 5, 6]
+// const array = [1, 2, 3, 4, 5, 6]
 
-const parImpar = array.map((elemento,index,array)=>{
-    if(elemento%2===0){
-        return `${elemento} é par`
-    }
-    return `${elemento} é ímpar`
-})
+// const parImpar = array.map((elemento,index,array)=>{
+//     if(elemento%2===0){
+//         return `${elemento} é par`
+//     }
+//     return `${elemento} é ímpar`
+// })
 
-console.log(parImpar)
+// console.log(parImpar)
+
+/*NÚMERO 3*/
+/*LETRA A*/
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+// const pessoasPermitidas = pessoas.filter((elemento,index,array)=>{
+//     if((elemento.altura >= 1.5) && (elemento.idade > 14) && (elemento.idade < 60)){
+//         return true
+//     }
+//     return false
+// })
+
+// console.log(pessoasPermitidas)
+
+/*LETRA B*/
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+// const pessoasNaoPermitidas = pessoas.filter((elemento,index,array)=>{
+//     if((elemento.altura >= 1.5) && (elemento.idade > 14) && (elemento.idade < 60)){
+//         return false
+//     }
+//     return true
+// })
+
+// console.log(pessoasNaoPermitidas)
