7229fa82be4d49e3df11199bb02b13fb2eb4a9be
Kamila Ribeiro Melo <kamilamelo5@hotmail.com>
2020-06-18 15:05:17 -0300

    Listas e Estilização

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,10 +3,31 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  
+  state = {
+    posts: [
+    {nomeUsuario: 'Kamila', fotoUsuario: 'https://picsum.photos/50/50', fotoPost:'https://picsum.photos/200/150'},
+    {nomeUsuario: 'João', fotoUsuario: 'https://picsum.photos/50/51', fotoPost: 'https://picsum.photos/200/151'},
+    {nomeUsuario: 'Maria', fotoUsuario: "https://picsum.photos/50/52",fotoPost: "https://picsum.photos/200/152"}
+    ]
+  }
+  
+  
   render() {
+
+    const listaDePosts = this.state.posts.map((post)=>{
+      return(
+        <div>
+          <p>{post.nomeUsuario}</p>
+          <p>{post.fotoPost}</p>
+          <p>{post.fotoUsuario}</p>
+        </div>
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
+        {/* <Post
           nomeUsuario={'Kamila'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
@@ -20,7 +41,8 @@ class App extends React.Component {
           nomeUsuario={'Maria'}
           fotoUsuario={'https://picsum.photos/50/52'}
           fotoPost={'https://picsum.photos/200/152'}
-        />
+        /> */}
+        <div>{listaDePosts}</div>
       </div>
     );
   }
